43ef6c09e10350dc30c3880489d98ced21aa3b2f
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2024-05-02 18:54:52 +0100

    feature: (#4)
    define relation between models

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
@@ -19,4 +20,12 @@ class Document extends Model
         'size',
         'url'
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function folder(): BelongsTo
+    {
+        return $this->belongsTo(Folder::class, 'folder_id', 'id');
+    }
 }
