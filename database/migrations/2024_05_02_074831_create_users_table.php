44c6f41659c7ad3d99f96ff3f041a36bc90f7891
arabelhousseyn <elhousseynarab@gmail.com>
2024-05-18 21:15:45 +0100

    enhancements

--- database/migrations/2024_05_02_074831_create_users_table.php
+++ database/migrations/2024_05_02_074831_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->boolean('is_admin')->default(false);
-            $table->foreignUuid('company_id')->constrained('companies')->cascadeOnDelete();
+            $table->foreignUuid('company_id')->nullable()->constrained('companies')->cascadeOnDelete();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
